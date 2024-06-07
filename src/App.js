8f16984b944da15f124090a6081123e370a14b49
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2024-06-07 22:12:25 +0500

    etid api

--- src/App.js
+++ src/App.js
@@ -24,9 +24,82 @@ function App() {
   const [showPlasticPayment, setShowPlasticPayment] = useState(false);
 
   const productOptions = [
-    { id: 'laminat', label: 'Laminat', subOptions: [{ id: 'type1', label: 'Type 1' }, { id: 'type2', label: 'Type 2' }] },
-    { id: 'akrel', label: 'Akrel', subOptions: [{ id: 'type3', label: 'Type 3' }, { id: 'type4', label: 'Type 4' }] },
-    // Add more products and sub-options as needed
+    { id: 'laminat', label: 'Laminat', subOptions: [
+      { id: 'AntikOch', label: 'Antik Och' }, 
+      { id: 'AntikSridniy', label: 'AntikSridniy' },
+      { id: 'AntikTuq', label: 'Antik Tuq' },
+      { id: 'ArushaOch', label: 'Arusha Och' },
+      { id: 'ArushaTuq', label: 'Arusha Tuq' },
+      { id: 'Atlas', label: 'Atlas' },
+      { id: 'Avstraliya', label: 'Avstraliya' },
+      { id: 'Aziya', label: 'Aziya' },
+      { id: 'BelayaKarona', label: 'BelayaKarona' },
+      { id: 'Efes', label: 'Efes' },
+      { id: 'Ekko', label: 'Ekko' },
+      { id: 'Enoks', label: 'Enoks' },
+      { id: 'Flarinso', label: 'Flarinso' },
+      { id: 'KaspiyTuq', label: 'KaspiyTuq' },
+      { id: 'LiftTuq', label: 'LiftTuq' },
+      { id: 'Lipa', label: 'Lipa' },
+      { id: 'LoftOch', label: 'LoftOch' },
+      { id: 'Luna', label: 'Luna' },
+      { id: 'Marmar', label: 'Marmar' },
+      { id: 'Minji', label: 'Minji' },
+      { id: 'Mokriy', label: 'Mokriy' },
+      { id: 'Oq', label: 'Oq' },
+      { id: 'OqShagniy', label: 'OqShagniy' },
+      { id: 'Paladina', label: 'Paladina' },
+      { id: 'Picar', label: 'Picar' },
+      { id: 'Qaymoq', label: 'Qaymoq' },
+      { id: 'Qora', label: 'Qora' },
+      { id: 'QoraShagniy', label: 'QoraShagniy' },
+      { id: 'Salativiy', label: 'Salativiy' },
+      { id: 'Sanama', label: 'Sanama' },
+      { id: 'Sofiya', label: 'Sofiya' },
+      { id: 'Stalnoy', label: 'Stalnoy' },
+      { id: 'Vatan', label: 'Vatan' },
+      { id: 'YangiRang', label: 'YangiRang' },
+      { id: 'Yasin', label: 'Yasin' },
+      { id: 'Yukon', label: 'Yukon' },
+      { id: 'kaspiyOch', label: 'Kaspiy Och' },
+      { id: 'mokriyShagrin', label: 'Mokriy Shagrin' },
+      { id: 'oqMatve', label: 'Oq Matve' },
+      { id: 'qizil', label: 'qizil' },
+     
+    ] },
+
+    { id: 'DSP', label: 'DSP', subOptions: [
+      { id: '5kv', label: '5kv' }, 
+      { id: '6kv', label: '5kv' }
+    ] },
+    { id: 'Akrel', label: 'Akrel', subOptions: [
+      { id: 'Akrel', label: 'Akrel' }, 
+    ] },
+    { id: 'Doska', label: 'Doska', subOptions: [
+      { id: 'Doska', label: 'Doska' }, 
+    ] },
+    { id: 'XDF', label: 'XDF', subOptions: [
+      { id: 'XDF', label: 'XDF' }, 
+    ] },
+
+    { id: 'MDF', label: 'MDF', subOptions: [
+      { id: 'AntikSridniyQimmat', label: 'Antik Sridniy Qimmat' }, 
+      { id: 'AntikTuq', label: 'Antik Tuq' },
+      { id: 'Arusha Och', label: 'Arusha Och' },
+      { id: 'ArushaTuqArzon', label: 'Arusha Tuq Arzon' },
+      { id: 'ArushaTuqQimmat', label: 'Arusha Tuq Qimmat' },
+      { id: 'Astana', label: 'Astana' },
+      { id: 'Atlas', label: 'Atlas' },
+      { id: 'Avstraliya', label: 'Avstraliya' },
+      { id: 'Aziya', label: 'Aziya' },
+      { id: 'Marmar', label: 'Marmar' },
+      { id: 'Oq', label: 'Oq' },
+      { id: 'Qaymoq', label: 'Qaymoq' },
+      { id: 'Sanam', label: 'Sanam' },
+      { id: 'Marmar', label: 'Marmar' },
+      { id: 'Marmar', label: 'Marmar' },
+    ] },
+    
   ];
 
   const calculateAndDisplayResult = () => {
